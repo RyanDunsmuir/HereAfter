ecaf42eecb865f368636df3f6e586147ab3dcf0a
Ryan Dusmuir <ryan_dunsmuir@kansamples.com>
2021-06-08 13:04:29 +0100

    edited pages controller and added routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/inbox' => 'pages#inbox', :as => :inbox
+  get '/capsules' => 'capsules#index', :as => :capsules
+  get '/capsule/:id' => 'capsules#destroy', :as => :capsule
+  get '/badges' => 'badges#index', :as => :badges
+
 end
