5425b3a9b369c6d06faf3b3ee704adc895cfc71f
Lucas Leite <lucasleite91@gmail.com>
2021-06-09 16:04:45 +0100

    added modal for creation of the capsule

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,24 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :inbox ]
+  skip_before_action :authenticate_user!, only: [:inbox]
 
   def home
     @inboxcaps = current_user.capsules.order(:arrival_date).reverse
+    @categories = Category.all
+    @users = get_user_list(User.all)
   end
 
   def inbox
     @capsules = current_user.capsules
     @owned_capsules = current_user.owned_capsules
+    @categories = Category.all
+    @users = get_user_list(User.all)
+  end
 
+  private
+
+  def get_user_list(users)
+    users.map do |user|
+      "#{user.first_name} #{user.last_name} - #{user.username}"
+    end.sort
   end
 end
