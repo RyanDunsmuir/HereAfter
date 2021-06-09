41c828f079f15b760ee1b0a03d736854a17bd8dd
Ryan Dusmuir <ryan_dunsmuir@kansamples.com>
2021-06-09 15:13:49 +0100

    added countdown timer js

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,10 +2,12 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :inbox ]
 
   def home
+    @inboxcaps = current_user.capsules.order(:arrival_date).reverse
   end
 
   def inbox
     @capsules = current_user.capsules
     @owned_capsules = current_user.owned_capsules
+
   end
 end
---
+++ app/javascript/plugins/countdown.js
@@ -0,0 +1,38 @@
+const countdownTimer = () => {
+  let capsules = document.querySelectorAll('#countdown');
+
+  function renderTimer(capsuleCard){
+    var countDownDate = new Date(capsuleCard.dataset.arrival).getTime();
+    // var countDownDate = document.getElementById("arrival_date").value;
+    // Update the count down every 1 second
+    var x = setInterval(function() {
+
+      // Get today's date and time
+      var now = new Date().getTime();
+
+      // Find the distance between now and the count down date
+      var distance = countDownDate - now;
+
+      // Time calculations for days, hours, minutes and seconds
+      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      // Display the result in the element with id="countdown"
+      capsuleCard.innerHTML = days + "d " + hours + "h "
+      + minutes + "m " + seconds + "s ";
+
+      // If the count down is finished, write some text
+      if (distance < 0) {
+        clearInterval(x);
+        CapsuleCard.innerHTML = "ARRIVED";
+      }
+    }, 1000);
+  }
+
+  capsules.forEach(renderTimer);
+
+}
+
+export { countdownTimer }
