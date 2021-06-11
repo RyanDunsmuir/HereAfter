620c77a5543b4570e89cdf1a541d6302ab2bffc6
Ryan Dusmuir <ryan_dunsmuir@kansamples.com>
2021-06-11 15:26:44 +0100

    fixed sidebar navs

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:inbox]
+  before_action :set_users_and_categories, except: [:home]
 
   def home
     @inboxcaps = current_user.capsules.order(:arrival_date).reverse
@@ -23,11 +24,22 @@ class PagesController < ApplicationController
     end.compact.sort_by(&:arrival_date).reverse
 
     @inboxcaps = caps_arrived_unread + caps_not_arrived + caps_opened
+  end
 
-    @capsules = current_user.capsules
-    @owned_capsules = current_user.owned_capsules
-    @categories = Category.all
-    @users = get_user_list(User.all)
+  def incoming
+    @inboxcaps = current_user.capsules.map do |capsule|
+      capsule if capsule.arrival_date > Date.today
+    end.compact.sort_by(&:arrival_date)
+  end
+
+  def outgoing
+    @inboxcaps = current_user.owned_capsules
+  end
+
+  def opened
+    @inboxcaps = current_user.capsules.map do |capsule|
+      capsule if capsule.read == true
+    end.compact.sort_by(&:arrival_date).reverse
   end
 
   def test
@@ -39,5 +51,10 @@ class PagesController < ApplicationController
     users.map do |user|
       "#{user.first_name} #{user.last_name} - #{user.username}"
     end.sort
+  end
+
+  def set_users_and_categories
+    @categories = Category.all
+    @users = get_user_list(User.all)
   end
 end
