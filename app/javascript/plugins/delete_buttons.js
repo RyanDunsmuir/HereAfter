3d1e59d7f2275a1b889b7d63094f4599e824df3a
Ryan Dusmuir <ryan_dunsmuir@kansamples.com>
2021-06-17 11:05:25 +0100

    fixed things

--- app/javascript/plugins/delete_buttons.js
+++ app/javascript/plugins/delete_buttons.js
@@ -1,9 +1,23 @@
 import Swal from 'sweetalert2/dist/sweetalert2.js'
 import 'sweetalert2/dist/sweetalert2.min.css'
 
+const afterAlert = (link, result) => {
+  const authToken = document.querySelector('meta[name="csrf-token"]').content;
+
+  if (result.isConfirmed) {
+    fetch(link.href, {
+      method: link.dataset.method,
+      headers: {
+        'X-CSRF-Token': authToken
+      }
+    }).then(() => {});
+
+    link.parentElement.classList.add('card-fade');
+  }
+}
+
 const initDeleteButtons = () => {
   const links = document.querySelectorAll('.hidden-bin');
-  const authToken = document.querySelector('meta[name="csrf-token"]').content;
 
   links.forEach(link => {
     link.addEventListener('click', (event) => {
@@ -17,14 +31,7 @@ const initDeleteButtons = () => {
         confirmButtonText: 'Delete',
         showCancelButton: true,
       }).then(result => {
-        if (result.isConfirmed) {
-          fetch(link.href, {
-            method: link.dataset.method,
-            headers: {
-              'X-CSRF-Token': authToken
-            }
-          }).then(() => {});
-          link.parentElement.classList.add('card-fade');
+        afterAlert(link, result);
       });
     });
   });
